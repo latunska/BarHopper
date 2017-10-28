9ce4bbffd7c771b01dbda36a129229fbb900c9d2
Alex Latunski <latunska@gmail.com>
2017-10-28 18:46:19 -0400

    Merge branch 'Marc' of https://github.com/latunska/BarHopper into Exninja
    # Conflicts:
    #	.idea/vcs.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/acm_aka_the_best/barhopper/LoginActivity.java
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/values/dimens.xml
    #	build.gradle
    
    Added Facebook API hook

--- app/src/main/java/acm_aka_the_best/barhopper/LoginActivity.java
+++ app/src/main/java/acm_aka_the_best/barhopper/LoginActivity.java
@@ -3,26 +3,12 @@ package acm_aka_the_best.barhopper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import android.view.View;
-import android.view.ViewGroup;
-
-
 import android.widget.Button;
-import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
