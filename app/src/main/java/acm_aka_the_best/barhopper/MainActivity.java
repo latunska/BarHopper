e220e2278b85756e561cd871a091a7210729c32f
Airsick <corauh@umflint.edu>
2017-10-28 03:40:55 -0400

    first

---
+++ app/src/main/java/acm_aka_the_best/barhopper/MainActivity.java
@@ -0,0 +1,13 @@
+package acm_aka_the_best.barhopper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
