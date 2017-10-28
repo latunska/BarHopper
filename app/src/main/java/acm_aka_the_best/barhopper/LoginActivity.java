67ab4bfeb70ef302c253c1e19efddc98c4a075f2
Alex Latunski <latunska@gmail.com>
2017-10-28 18:24:19 -0400

    Merging of all projects

--- app/src/main/java/acm_aka_the_best/barhopper/LoginActivity.java
+++ app/src/main/java/acm_aka_the_best/barhopper/LoginActivity.java
@@ -3,6 +3,7 @@ package acm_aka_the_best.barhopper;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -82,6 +83,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     return true;
                 }
                 return false;
+            }
+        });
+
+        Button MainActivityButton = (Button) findViewById(R.id.main_activiy_button);
+
+        MainActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
