e12ea1758eeefcce39bd685194b512de72d48089
PunchedGRUNT <33170266+PunchedGRUNT@users.noreply.github.com>
2017-10-28 18:58:48 -0400

    Launch Icon added Action Bar removed

--- app/src/main/java/acm_aka_the_best/barhopper/BarMenu.java
+++ app/src/main/java/acm_aka_the_best/barhopper/BarMenu.java
@@ -1,5 +1,6 @@
 package acm_aka_the_best.barhopper;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,7 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,22 +52,34 @@ public class BarMenu extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        ListView lv = (ListView) getView().findViewById(R.id.barList);
 
-        // Instanciating an array list (you don't need to do this,
-        // you already have yours).
-        List<String> your_array_list = new ArrayList<String>();
-        your_array_list.add("foo");
-        your_array_list.add("bar");
+        //ListView lv = (ListView) getView().findViewById(R.id.barList);
 
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this.getActivity(),
-                android.R.layout.simple_list_item_1,
-                your_array_list );
+        //LinearLayout layout = (LinearLayout) view.findViewById(R.id.linear);
+        //LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+         //       ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
 
-        lv.setAdapter(arrayAdapter);
+
+       /* LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,100);
+        LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,50);
+
+        ArrayList<LinearLayout> elementFrameList = new ArrayList<LinearLayout>();
+        ArrayList<TextView> barNameList = new ArrayList<TextView>();
+        //ArrayList<LinearLayout> friendsAtBarList
+
+        for(int i=0; i<5; i++)
+        {
+            elementFrameList.add(new LinearLayout(this.getContext()));
+            barNameList.add(new TextView(this.getContext()));
+
+            elementFrameList.get(i).setLayoutParams(params);
+            barNameList.get(i).setLayoutParams(textParams);
+            barNameList.get(i).setText("test");
+
+            elementFrameList.get(i).addView(barNameList.get(i));
+            lv.(elementFrameList.get(i));
+        }
+        */
+
     }
 }
--- app/src/main/java/acm_aka_the_best/barhopper/DDMenu.java
+++ app/src/main/java/acm_aka_the_best/barhopper/DDMenu.java
@@ -1,12 +1,21 @@
 package acm_aka_the_best.barhopper;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DDMenu extends Fragment {
@@ -34,6 +43,24 @@ public class DDMenu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_ddmenu, container, false);
+
+
+
         return rootView;
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        Button uberButton  = (Button) this.getView().findViewById(R.id.uber_button);
+
+        uberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //EVAN! ADD UBER STUFF HERE
+                Toast toast = Toast.makeText(getActivity().getApplicationContext(),"It works",Toast.LENGTH_SHORT);
+                        toast.show();
+
+            }
+        });
     }
 }
