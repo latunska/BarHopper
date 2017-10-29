f294b1969f9489726b48a134e29051718b401360
BuffloMildWings <edembo@umflint.edu>
2017-10-28 23:58:13 -0400

    Addimg the UBER SDK

--- app/src/main/java/acm_aka_the_best/barhopper/DDMenu.java
+++ app/src/main/java/acm_aka_the_best/barhopper/DDMenu.java
@@ -1,13 +1,22 @@
 package acm_aka_the_best.barhopper;
 
 import android.content.Context;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Dimension;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.android.rides.RideParameters;
+import com.uber.sdk.android.rides.RideRequestButton;
 
 public class DDMenu extends Fragment {
     /**
@@ -35,5 +44,20 @@ public class DDMenu extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_ddmenu, container, false);
         return rootView;
+    }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        RideRequestButton requestButton = new RideRequestButton(getContext());
+        RelativeLayout layout = (RelativeLayout)  this.getView().findViewById(R.id.uber_request_button);
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //EVAN! ADD UBER STUFF HERE
+                UberSdk.initialize(config);
+               // Toast toast = Toast.makeText(getActivity().getApplicationContext(),"It works", Toast.LENGTH_SHORT);
+              //  toast.show();
+
+
+            }
+        });
     }
 }
--- app/src/main/java/acm_aka_the_best/barhopper/UberSdk.java
+++ app/src/main/java/acm_aka_the_best/barhopper/UberSdk.java
@@ -1,30 +1,27 @@
 package acm_aka_the_best.barhopper;
 
-// import com.uber.sdk.android.core.UberSdk;
-import com.uber.sdk.core.auth.Scope;
-import com.uber.sdk.rides.client.SessionConfiguration;
-
 /**
  * Created by Evan on 10/28/2017.
  */
+import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.core.auth.Scope;
+import com.uber.sdk.rides.client.SessionConfiguration;
+import com.uber.sdk.core.auth.Authenticator;
+import com.uber.sdk.core.auth.internal.OAuth2Service;
+import com.uber.sdk.android.core.BuildConfig;
+import com.uber.sdk.rides.client.services.RidesService;
+import com.uber.sdk.rides.client.*;
+import com.uber.sdk.core.*; // This I THINK imports everything starting with that import line. -E
+import com.uber.sdk.android.core.UberButton;
 
-public class UberSdk implements barhopper {
-    public UberSdk(SessionConfiguration config) {
-        this.config = config;
-    }
+// More imports needed.############### Use this process to get the missing bits https://developer.uber.com/docs/riders/ride-requests/tutorials/button/android
 
+ class UberSdk {
     SessionConfiguration config = new SessionConfiguration.Builder()
-            // mandatory
-            .setClientId("HLD_GUi2LB9TnJbALAjQqYBnGL_qp9C7")
-            // required for enhanced button features
+            .setClientId("HLD_GUi2LB9TnJbALAjQqYBnGL_qp9C7") //This is necessary
             .setServerToken("X5KdrYbRlAeLBY51895qu-elOmrDneRWzilEFbx5")
-            // required for implicit grant authentication
-            //.setRedirectUri("<REDIRECT_URI>")
-            // required scope for Ride Request Widget features
-            //.setScopes(Arrays.asList(Scope.RIDE_WIDGETS))
-            // optional: set sandbox as operating environment
-            //.setEnvironment(SessionConfiguration.Environment.SANDBOX)
+            //.setRedirectUri("") //This is necessary if you'll be using implicit grant
+           // .setEnvironment(Environment.SANDBOX) //Useful for testing your app in the sandbox environment
+           // .setScopes(Arrays.asList(Scope.PROFILE, Scope.RIDE_WIDGETS)) //Your scopes for authentication here
             .build();
-// initialize the SDK with the config object:
-
 }
